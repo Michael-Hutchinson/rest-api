4d02c54325666e0635f395d4343b8697339084ca
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-11-09 14:31:20 +0000

    fix: spelling and export update user

--- src/db/users.ts
+++ src/db/users.ts
@@ -13,13 +13,13 @@ const UserSchema = new mongoose.Schema({
 export const UserModel = mongoose.model('User', UserSchema);
 
 export const getUsers = () => UserModel.find();
-export const getUsersByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
 export const getUserBySessionToken = (sessionToken: string) =>
-  UserModel.findOne({ 'autentication.sessionToken': sessionToken });
+  UserModel.findOne({ 'authentication.sessionToken': sessionToken });
 export const getUserById = (id: string) => UserModel.findById(id);
 export const createUser = (values: Record<string, any>) =>
   new UserModel(values).save().then((user) => user.toObject());
 export const deleteUserById = (id: string) =>
-  UserModel.findByIdAndDelete({ _id: id });
-const updateUserById = (id: string, values: Record<string, any>) =>
+  UserModel.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) =>
   UserModel.findByIdAndUpdate(id, values);
