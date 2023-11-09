1907ffb02a4d9fc355c18c91efe355edad4c432c
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-11-09 14:30:26 +0000

    feat: create user actions

--- src/db/users.ts
+++ src/db/users.ts
@@ -9,3 +9,17 @@ const UserSchema = new mongoose.Schema({
     sessionToken: { type: String, select: false },
   },
 });
+
+export const UserModel = mongoose.model('User', UserSchema);
+
+export const getUsers = () => UserModel.find();
+export const getUsersByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserBySessionToken = (sessionToken: string) =>
+  UserModel.findOne({ 'autentication.sessionToken': sessionToken });
+export const getUserById = (id: string) => UserModel.findById(id);
+export const createUser = (values: Record<string, any>) =>
+  new UserModel(values).save().then((user) => user.toObject());
+export const deleteUserById = (id: string) =>
+  UserModel.findByIdAndDelete({ _id: id });
+const updateUserById = (id: string, values: Record<string, any>) =>
+  UserModel.findByIdAndUpdate(id, values);
