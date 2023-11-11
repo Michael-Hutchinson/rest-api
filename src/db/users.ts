8f472fd93dce6d6982c0527e3a845a6c36911f97
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-11-11 16:52:25 +0000

    fix: spelling mistake causing post request to fail

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
   email: { type: String, required: true },
-  autentication: {
+  username: { type: String, required: true },
+  authentication: {
     password: { type: String, required: true, select: false },
     salt: { type: String, select: false },
     sessionToken: { type: String, select: false },
