851d161a9914b7a7e15fd57674edeaa82b1faf7e
Michael-Hutchinson <michael-hutchinson@hotmail.co.uk>
2023-11-11 19:55:30 +0000

    feat: create route and controller for updating a user

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { deleteUser, getAllUsers } from '../controllers/users';
+import { deleteUser, getAllUsers, updateUser } from '../controllers/users';
 import { isAuthenticated, isUser } from '../middlewares';
 
 export default (router: express.Router) => {
   router.get('/users', isAuthenticated, getAllUsers);
   router.delete('/users/:id', isAuthenticated, isUser, deleteUser);
+  router.patch('/users/:id', isAuthenticated, isUser, updateUser);
 };
